8a3e44c80e75daf923b91ebfa60379883793309e
luciano.federico.olmedo <luciano.federico.olmedo@gmail.com>
2016-06-05 19:03:15 -0300

    Adding Circle CI

---
+++ app/src/androidTest/java/com/ingsoft/magic_assistant/TestActivityTest.java
@@ -0,0 +1,22 @@
+package com.ingsoft.magic_assistant;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.runner.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class TestActivityTest {
+
+    @Rule public final ActivityRule<HomeActivity> home = new ActivityRule<>(HomeActivity.class);
+
+    @Test
+    public void shouldBeAbleToLaunchHomeScreen(){
+        onView(withText("Contadores de vidas")).check(ViewAssertions.matches(isDisplayed()));
+    }
+}
--- app/src/test/java/ingsoft/com/magic_assistant/ExampleUnitTest.java
+++ app/src/test/java/ingsoft/com/magic_assistant/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.mobaires.gymdroid;
+package com.ingsoft.magic_assistant;
 
 import org.junit.Test;
 
@@ -10,6 +10,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertEquals(5, 2 + 2);
     }
 }
